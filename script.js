f9795f942c41207f814de009e26fb7c947eb8cf0
lewisthagichu <lewisthagichu254@gmail.com>
2023-07-24 09:27:57 +0300

    Refactor code to ensure restart button works as intended

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var originBoard;
+let originBoard;
 let aiTurn = true;
 let xScore = 0;
 let circleScore = 0;
@@ -19,55 +19,40 @@ const cellElements = document.querySelectorAll(".data-cell");
 const endRound = document.querySelector(".end-round");
 const restartBtn = document.getElementById("restart-btn");
 
-const startgame = (() => {
+restartBtn.addEventListener("click", restart);
+startGame();
+
+function startGame() {
   originBoard = Array.from(Array(9).keys());
   cellElements.forEach((cell) => {
     cell.addEventListener("click", handleClick, { once: true });
   });
-})();
-
-const restart = (() => {
-  restartBtn.addEventListener("click", () => {
-    document.querySelector("#x-score").textContent = "0";
-    document.querySelector("#circle-score").textContent = "0";
-    document.querySelector("#message").textContent = "";
-    endRound.style.display = "none";
-    startgame();
-  });
-})();
+}
 
 function handleClick(e) {
   const cell = e.target;
   const currentPlayer = aiTurn ? aiPlayer : huPlayer;
-  console.log(currentPlayer);
-
   const cellId = e.target.id;
 
   placeMark(cell, currentPlayer, cellId);
 
-  //check winner
   if (checkWinner(originBoard, currentPlayer)) {
     displayResult(currentPlayer);
   }
+  checkDraw();
 
-  //switch turns
   switchTurns();
 }
 
-//place marks on the cell
 function placeMark(cell, currentPlayer, cellId) {
   cell.classList.remove("circle");
   cell.classList.remove("x");
   originBoard[cellId] = currentPlayer;
 
-  if (currentPlayer == huPlayer) {
-    cell.classList.add("circle");
-  } else {
-    cell.classList.add("x");
-  }
+  if (currentPlayer == huPlayer) cell.classList.add("circle");
+  cell.classList.add("x");
 }
 
-// check winner
 function checkWinner(board, player) {
   const plays = board.reduce(
     (acc, arr, i) => (arr === player ? acc.concat(i) : acc),
@@ -81,17 +66,58 @@ function checkWinner(board, player) {
 
 function displayResult(player) {
   player == huPlayer ? circleScore++ : xScore++;
-  updateScore(player);
+  updateScore(`${getCurrentPlayer()} WINS THE ROUND`);
 }
 
-function updateScore(player) {
+function updateScore(message) {
   document.querySelector("#x-score").textContent = xScore;
   document.querySelector("#circle-score").textContent = circleScore;
-  document.querySelector("#message").textContent = `${player} WINS THE ROUND`;
+  declareWinner(message);
   endRound.style.display = "flex";
 }
 
-// switch turns between players
+function declareWinner(message) {
+  document.querySelector("#message").textContent = message;
+}
+
+function getCurrentPlayer() {
+  return aiTurn ? aiPlayer : huPlayer;
+}
+
 function switchTurns() {
   aiTurn = !aiTurn;
+  switchDash();
+}
+
+function switchDash() {
+  document.querySelector(".icon.circle").classList.toggle("dash");
+  document.querySelector(".icon.x").classList.toggle("dash");
+}
+
+function checkDraw() {
+  availSpaces(originBoard);
+  if (availSpaces(originBoard) === 0) {
+    updateScore("IT'S A DRAW");
+  }
+}
+
+function availSpaces(board) {
+  return board.reduce((acc, arr) => {
+    return typeof arr === "number" ? acc + 1 : acc;
+  }, 0);
+}
+
+function restart() {
+  xScore = 0;
+  circleScore = 0;
+  document.querySelector("#x-score").textContent = 0;
+  document.querySelector("#circle-score").textContent = 0;
+  document.querySelector("#message").textContent = "";
+  endRound.style.display = "none";
+
+  cellElements.forEach((cell) => {
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+  });
+  startGame();
 }
